1b1ae1787d15553379300d96656e3a273e87d33c
Andreas Skotadis <skottydeveloper@gmail.com>
2022-02-05 22:15:07 +1100

    Upload hello world mini-project.

---
+++ mini_projects/Initials.swift
@@ -0,0 +1,21 @@
+/* This is a mere "hello world" mini-project to revise printing in Swift. 
+To view mini-projects of more complexity and purpose, please view the later mini-projects. */
+
+print(" *                *            * ")
+print("      *     *                    ")
+print("                      *          ")
+print("*         *                 *    ")
+print("     *           *     *         ")
+print("           HHHHHHH        *      ")
+print(" *         HHHHHHH               ")
+print("     *   HHHHHHHHHHH    *     *  ")
+print("         *   @ @   *             ")
+print("        *     ^     *            ")
+print("  *       *       *         *    ")
+print("         SSS * * S               ")
+print("         * SSSSSSSS *            ")
+print("       *      O   SS  *          ")
+print("      *       O    S   *         ")
+print("      *       O        *         ")
+print("       *              *          ")
+print("           *   *   *             ")
