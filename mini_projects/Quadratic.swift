d0de3cd18011315d6711039b44ee96a6ba047a26
Andreas Skotadis <skottydeveloper@gmail.com>
2022-02-06 15:27:55 +1100

    Upload Quadratic mini-project.

---
+++ mini_projects/Quadratic.swift
@@ -0,0 +1,13 @@
+// This basic program solves the quadratic equation for "x".
+var a: Double = 2.0
+var b: Double = 5.0
+var c: Double = 3.0
+
+var root1: Double
+var root2: Double
+
+root1 = (-b + (b*b - 4*a*c).squareRoot()) / (2*a)
+root2 = (-b - (b*b - 4*a*c).squareRoot()) / (2*a)
+
+print("Root 1: \(root1)")
+print("Root 2: \(root2)")
