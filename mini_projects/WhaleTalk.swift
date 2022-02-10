261edaa3547103438092ad3c19028d2bdcac6d8e
Andreas Skotadis <skottydeveloper@gmail.com>
2022-02-10 23:25:38 +1100

    Upload WhaleTalk mini-project.

---
+++ mini_projects/WhaleTalk.swift
@@ -0,0 +1,18 @@
+var input = "whale talk"
+var output = ""
+
+for char in input {
+  let lowerChar = char.lowercased()
+
+  switch lowerChar {
+    case "a", "i", "o":
+      output += lowerChar.uppercased()
+    case "e":
+      output += "EE"
+    case "u":
+      output += "UU"
+    default:
+      continue    
+  }
+}
+print(output)
