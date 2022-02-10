e5fa1caa5bbe92693b906ef5304dd4831ef46ea7
Andreas Skotadis <skottydeveloper@gmail.com>
2022-02-10 23:34:54 +1100

    Upload Bottles mini-project.

---
+++ mini_projects/Bottles.swift
@@ -0,0 +1,9 @@
+// Write your code below 🥛
+var numBottles: Int = 99
+
+for numBottles in stride(from: 99, to: 0, by: -1) {
+  print("\(numBottles) bottles of milk on the wall, \(numBottles) bottles of milk!")
+  print("You take one down and pass it around...")
+  print("\(numBottles-1) bottles of milk on the wall!")
+}
+print("0 bottles of milk on the wall, 0 bottles of milk!\nGo to the store and buy some more,\n99 bottles of milk on the wall!")
